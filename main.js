b72ff9186e4f325441c1bacf80b9353a6e86dc97
Micka <baguet.mickael@gmail.com>
2019-04-08 16:02:47 +0200

    ajout isNaN

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 // Fonction divison deux nombre puis affiche le reste de la division
 function division()
 {
-  // Méthode détaillée :
-  // var nb1 = document.getElementById('firstNumber').value;
-  // var nb2 = document.getElementById('secondNumber').value;
+  // Si l'une des deux valeurs n'est pas un nombre :
+  if (isNaN(firstNumber.value) || isNaN(secondNumber.value))
+  {
+    alert("Veuillez entrer des nombres !")
+  }
+  // Sinon faire le code normalement
+  else
+  {
+    // Méthode détaillée :
+    // var nb1 = document.getElementById('firstNumber').value;
+    // var nb2 = document.getElementById('secondNumber').value;
 
-  // Utilisation du % (modulo) afin de prendre le reste de la division
-  // alert("Reste de la division : " + nb1 % nb2);
+    // Utilisation du % (modulo) afin de prendre le reste de la division
+    // alert("Reste de la division : " + nb1 % nb2);
 
-  // Méthode réduite : toujours avec le % 
-  alert("Reste de la division : " + firstNumber.value % secondNumber.value);
+    // Méthode réduite : toujours avec le %
+    alert("Reste de la division : " + firstNumber.value % secondNumber.value);
+  }
+
 }
