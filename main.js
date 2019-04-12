f498f4f1723cd0a9abb831f7a12bec0078395c03
Micka <baguet.mickael@gmail.com>
2019-04-12 08:52:31 +0200

    correction

--- main.js
+++ main.js
@@ -10,14 +10,13 @@ function division()
   else
   {
     // Méthode détaillée :
-    // var nb1 = document.getElementById('firstNumber').value;
-    // var nb2 = document.getElementById('secondNumber').value;
-
+    var nb1 = document.getElementById('firstNumber').value;
+    var nb2 = document.getElementById('secondNumber').value;
     // Utilisation du % (modulo) afin de prendre le reste de la division
-    // alert("Reste de la division : " + nb1 % nb2);
+    alert("Reste de la division : " + nb1 % nb2);
 
     // Méthode réduite : toujours avec le %
-    alert("Reste de la division : " + firstNumber.value % secondNumber.value);
+    alert("Reste de la division : " + document.getElementById('firstNumber').value % document.getElementById('secondNumber').value);
   }
 
 }
